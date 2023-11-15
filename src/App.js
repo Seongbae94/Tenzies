e26d2a139d02d98369f900adc484f5655721c3d0
Seongbae94 <seongbaelee944@gmail.com>
2023-11-15 17:21:12 -0500

    add skeleton of component
    1. add die component
    2. add some styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Die from "./components/Die";
+
 function App() {
-  return <main>hihi</main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
