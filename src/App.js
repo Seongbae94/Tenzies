87e446f6df92615ee2aaa31de3a01cf53f36a647
Seongbae94 <seongbaelee944@gmail.com>
2023-11-15 18:13:21 -0500

    add logics
    1. able to hold values
    2. able to get random values

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
+import { useEffect, useState } from "react";
 import Die from "./components/Die";
 
 function App() {
+  const [randomDiceArr, setRandomDiceArr] = useState();
+  const [count, setCount] = useState(0);
+
+  const randomDiceNumber = () => {
+    let generatedDiceArr = [];
+
+    for (let i = 0; i < 10; i++) {
+      generatedDiceArr.push({
+        value: Math.ceil(Math.random() * 6),
+        isheld: false,
+        id: i + 1,
+      });
+    }
+
+    setRandomDiceArr(generatedDiceArr);
+  };
+
+  const RollClickHandler = () => {
+    setCount((prev) => prev + 1);
+    setRandomDiceArr((prev) =>
+      prev.map((dice) =>
+        dice.isheld
+          ? { ...dice }
+          : { ...dice, value: Math.ceil(Math.random() * 6) }
+      )
+    );
+  };
+
+  const DiceClickHandler = (id) => {
+    setRandomDiceArr((prev) =>
+      prev.map((dice) =>
+        dice.id === id ? { ...dice, isheld: !dice.isheld } : { ...dice }
+      )
+    );
+  };
+
+  useEffect(() => {
+    randomDiceNumber();
+  }, []);
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {randomDiceArr &&
+          randomDiceArr.map((dice, index) => {
+            return (
+              <Die
+                key={index}
+                value={dice.value}
+                id={dice.id}
+                isheld={dice.isheld}
+                DiceClickHandler={DiceClickHandler}
+              />
+            );
+          })}
       </div>
+      <button className="roll-dice" onClick={() => RollClickHandler()}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-const Die = ({ value }) => {
+const Die = ({ value, DiceClickHandler, id, isheld }) => {
+  const styles = {
+    backgroundColor: isheld ? "#59E391" : "white",
+  };
+
   return (
-    <div className="die-face">
+    <div
+      className="die-face"
+      style={styles}
+      onClick={() => DiceClickHandler(id)}
+    >
       <h2 className="die-num">{value}</h2>
     </div>
   );
